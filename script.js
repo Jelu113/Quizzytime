e4eb2b88b4ac8bb684285a9f8d6574665fe8edca
jelu <jenaehe@yahoo.com>
2023-07-12 21:45:25 -0600

    updating code

--- script.js
+++ script.js
@@ -1,92 +1,76 @@
+
+
 var questions = [{
 
-    question1: "What is not one of the basic Data Types in Javascript?",
-    a: [{text:"Boolean", isCorrect: false},
-    {text: "String", isCorrect: false},
-    {text:"Array", isCorrect: true},
-    {text:"Number", isCorrect: false}
-    ]
-    },
-    {
+    question: "What is not one of the basic Data Types in Javascript?",
+    answers: ["Boolean", "String", "Array", "Number"],
+    correctAnswer: "Array"
 
+},
 
-    question2:"What symbol is used to assign a variable?",
-    a: [{text: "<", isCorrect: false},
-    {text: "=", isCorrect: true},
-    {text: "*", isCorrect: false},
-    {text: "&", isCorrect: false},
-    ]
-    },
-    {
+{
 
-    question3: "What is this an example of? answerButton",
-    a:[{text: "A variable", isCorrect: false},
-    {text: "An object", isCorrect: false},
-    {text: "An array", isCorrect: false},
-    {text: "Camel casing", isCorrect: true},
-    ]
-    }
-    ]
+    question: "What symbol is used to assign a variable?",
+    answers: ["=", "*", "&", "<"],
+    correctAnswer: "="
+},
+{
+    question: "What is this an example of? answerButton",
+    answers: ["A variable", "An object", "An array", "Camel casing"],
+    correctAnswer: "Camel casing"
 
-
-var timerEl = document.getElementById('#countdown') 
-var highscore= document.getElementById('#highscore')
-var start = document.getElementById("#btnStart")
-var buttonAnswer = document.querySelector(".btnAnswer")
-
-function countdown () {
-    var timeLeft = 60;
-
-    var timeInterval = setInterval(function(){
-        if (timeLeft >= 0) {
-            timerEl.textContent = "Time: " + timeLeft;
-            timeLeft--
-             
-        }
-        
-
-
-       /* else if(boolean, string, number === true){
-            timeLeft - 10
-        }
-        else {
-            timerEl.textContent ='';
-            clearInterval(timeInterval);
-            confirm("You loose")
-            console.log ("Lost")
-            confirm("Would you like to play again?")
-          
-            if (confirm("Would you like to play again?")){
-            console.log ("Yes");
-            window.location.reload();
-        } else {
-            console.log ("No");
-            window.location.reload();
-        }*/
-          
-        
-}, 1000);
-    
-}
-countdown ()
-
-
-
-
-function buttonClick() {
-    var boolean = document.getElementById('boolean')
-    var string = document.getElementById('string')
-    var number = document.getElementById('number')
-    var array = document.getElementById("array")
-    //var button = doument.getElement("button")
-     //boolean.value = button;
-    if(boolean === true) {
-         timeLeft -10
-    }
-
-    if(string === true) {
-        timeLeft -10
-    }
 }
 
-boolean.addEventListener("click", buttonClick)
+]
+
+
+var timerEl = document.getElementById('countdown')
+var highscore = document.getElementById('highscore')
+var start = document.getElementById("btnStart")
+var buttonAnswer = document.querySelectorAll(".btnAnswer")
+var questionIndex = 0
+
+function displayQuestion() {
+    var question = document.querySelector(".question")
+    question.textContent = questions[questionIndex].question
+
+    var btnAnswer1 = document.querySelector(".btnAnswer1")
+    btnAnswer1.textContent = questions[questionIndex].answers[0]
+
+    var btnAnswer2 = document.querySelector(".btnAnswer2")
+    btnAnswer2.textContent = questions[questionIndex].answers[1]
+
+    var btnAnswer3 = document.querySelector(".btnAnswer3")
+    btnAnswer3.textContent = questions[questionIndex].answers[2]
+
+    var btnAnswer4 = document.querySelector(".btnAnswer4")
+    btnAnswer4.textContent = questions[questionIndex].answers[3];
+
+    function countdown() {
+        var timeLeft = 60;
+        var timeInterval = setInterval(function () {
+
+            if (timeLeft >= 0) {
+                timerEl.textContent = "Time: " + timeLeft;
+                timeLeft--;
+
+            } else {
+                timerEl.textContent = "";
+                clearInterval(timeInterval);
+                // displayMessage() //create function to say they lost and ask to play again
+            }
+
+        }, 1000);
+
+    }
+    countdown()
+}
+
+
+//create a function to -10 seconds the time for wrong answers
+//create way to store the correct and incorrect answers and display them
+//creat a next button or a way to move from one screen to the next. 
+start.addEventListener("click", displayQuestion);
+
+// for (var i = 0; i = buttonAnswer; i++)
+//    .addEventListener ("click", btnAnswer)
