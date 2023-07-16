133b263ae051a178e8292f8105a9bfe573862e1f
jelu <jenaehe@yahoo.com>
2023-07-16 10:02:48 -0600

    fixed the ending of the quiz

--- script.js
+++ script.js
@@ -1,103 +1,95 @@
-
-
-var questions = [{
-
-    question: "What is not one of the basic Data Types in Javascript?",
-    answers: ["Boolean", "String", "Array", "Number"],
-    correctAnswer: "Array"
-
-},
-{
-
-    question: "What symbol is used to assign a variable?",
-    answers: ["=", "*", "&", "<"],
-    correctAnswer: "="
-},
-{
-    question: "What is this an example of? answerButton",
-    answers: ["A variable", "An object", "An array", "Camel casing"],
-    correctAnswer: "Camel casing"
-
-}
-]
-
-var timerEl = document.getElementById('countdown')
-var highscore = document.getElementById('highscore')
-var start = document.getElementById("btnStart")
-var buttonAnswer = document.querySelectorAll(".btnAnswer")
-var question = document.querySelectorAll(".question")
-var questionIndex = 0
-var displayMessage = "Please Try again"
-
-var currentQuestion = questions[questionIndex];
-
-
-
-function displayQuestion() {
+var questions = [
+    {
+      question: "What is not one of the basic Data Types in Javascript?",
+      answers: ["Boolean", "String", "Array", "Number"],
+      correctAnswer: "Array",
+    },
+    {
+      question: "What symbol is used to assign a variable?",
+      answers: ["=", "*", "&", "<"],
+      correctAnswer: "=",
+    },
+    {
+      question: "What is this an example of? answerButton",
+      answers: ["A variable", "An object", "An array", "Camel casing"],
+      correctAnswer: "Camel casing",
+    },
+  ];
+  
+  var timerEl = document.getElementById("countdown");
+  var highscore = document.getElementById("highscore");
+  var start = document.getElementById("btnStart");
+  var buttonAnswer = document.querySelectorAll(".btnAnswer");
+  var question = document.querySelectorAll(".question");
+  var questionIndex = 0;
+  //var displayMessage = "Please Try again";
+  var score = 0;
+  var timeInterval;
+  
+  // When start button clicked, this loops through the questions using the submitAnswer function
+  function displayQuestion() {
     for (var i = 0; i < buttonAnswer.length; i++) {
-        buttonAnswer[i].addEventListener("click", submitAnswer);
+      buttonAnswer[i].addEventListener("click", submitAnswer);
     }
-    
-    var question = document.querySelector(".question")
-    question.textContent = questions[questionIndex].question
-
-    var btnAnswer1 = document.querySelector(".btnAnswer1")
-    btnAnswer1.textContent = questions[questionIndex].answers[0]
-
-    var btnAnswer2 = document.querySelector(".btnAnswer2")
-    btnAnswer2.textContent = questions[questionIndex].answers[1]
-
-    var btnAnswer3 = document.querySelector(".btnAnswer3")
-    btnAnswer3.textContent = questions[questionIndex].answers[2]
-
-    var btnAnswer4 = document.querySelector(".btnAnswer4")
+  
+    var question = document.querySelector(".question");
+    question.textContent = questions[questionIndex].question;
+  
+    var btnAnswer1 = document.querySelector(".btnAnswer1");
+    btnAnswer1.textContent = questions[questionIndex].answers[0];
+  
+    var btnAnswer2 = document.querySelector(".btnAnswer2");
+    btnAnswer2.textContent = questions[questionIndex].answers[1];
+  
+    var btnAnswer3 = document.querySelector(".btnAnswer3");
+    btnAnswer3.textContent = questions[questionIndex].answers[2];
+  
+    var btnAnswer4 = document.querySelector(".btnAnswer4");
     btnAnswer4.textContent = questions[questionIndex].answers[3];
-
-}
-
-function checkAnswer() {
-
-}
-// use event.target in an if statement to check if it matches questions[questionIndex].correctAnswer
-// if correct add to score
-// if incorrect don't add to score
-function submitAnswer(event) {
-    event.preventDefault()
-    console.log(event.target.textContent)
-    questionIndex++
-    displayQuestion()
-        
-     
- }
-    // displayMessage() //create function to say they lost and ask to play again
-    //choose correctAnswer and save that information
-    //create a function to -10 seconds the time for wrong answers
-    function countdown() {
-
-        var timeLeft = 60;
-        var timeInterval = setInterval(function () {
-
-            if (timeLeft >= 0) {
-                timerEl.textContent = "Time: " + timeLeft;
-                timeLeft--;
-
-            } else {
-                timerEl.textContent = "";
-                clearInterval(timeInterval);
-
-            }
-
-        }, 1000);
-
+  }
+  
+  function submitAnswer(event) {
+    event.preventDefault();
+    var selectedAnswer = event.target.textContent;
+    var currentQuestion = questions[questionIndex]; // Move this line here
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      score++;
     }
-
-
-    function callTwo() {
-        countdown()
-        displayQuestion()
+    questionIndex++;
+    if (questionIndex < questions.length) {
+      displayQuestion();
+    } else {
+      endQuiz();
     }
-
-    start.addEventListener("click", callTwo);
-    //buttonAnswer.addEventListener("click", correctAnswer)
-
-    console.log(buttonAnswer.length)
+  }
+  
+  function endQuiz() {
+    clearInterval(timeInterval);
+    var finalScore = score;
+    var endMessage = "Quiz ended. Your score is: " + finalScore;
+    console.log(endMessage);
+  }
+  
+  function countdown() {
+    var timeLeft = 60;
+    timeInterval = setInterval(function () {
+      if (timeLeft >= 0) {
+        timerEl.textContent = "Time: " + timeLeft;
+        timeLeft--;
+      } else {
+        timerEl.textContent = "";
+        clearInterval(timeInterval);
+        endQuiz();
+      }
+    }, 1000);
+  }
+  
+  function callTwo() {
+    countdown();
+    displayQuestion();
+    start.style.display ="none";
+  }
+  
+  start.addEventListener("click", callTwo);
+  
+  console.log(buttonAnswer.length);
